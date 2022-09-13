282e4a944b3c824b91ccd7891969d4996d5273fe
OscarGonzalez97 <oscaren_gonz0912@hotmail.com>
2022-09-13 00:45:32 -0400

    inicializacion de proyecto

---
+++ dta-porteria/src/main/java/com/roshka/dtaporteria/DtaPorteriaApplication.java
@@ -0,0 +1,13 @@
+package com.roshka.dtaporteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtaPorteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtaPorteriaApplication.class, args);
+	}
+
+}
---
+++ dta-porteria/src/test/java/com/roshka/dtaporteria/DtaPorteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roshka.dtaporteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtaPorteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
