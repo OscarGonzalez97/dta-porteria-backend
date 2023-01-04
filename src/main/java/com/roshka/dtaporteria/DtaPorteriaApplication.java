278595983902d75e7f2681023ee40a675e5a0de7
Diego <diegoyegrosa@gmail.com>
2023-01-04 16:27:46 -0300

    Clean DtaPorteriaApplication

--- src/main/java/com/roshka/dtaporteria/DtaPorteriaApplication.java
+++ src/main/java/com/roshka/dtaporteria/DtaPorteriaApplication.java
@@ -1,25 +1,7 @@
 package com.roshka.dtaporteria;
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.DocumentChange;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.Firestore;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.FirestoreClient;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.iid.FirebaseInstanceId;
 import com.roshka.dtaporteria.config.FirebaseConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.List;
-import java.util.Objects;
-
 @SpringBootApplication
 public class DtaPorteriaApplication {
 
