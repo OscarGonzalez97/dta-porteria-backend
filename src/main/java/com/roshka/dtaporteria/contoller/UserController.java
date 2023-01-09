154c973ba1301290b86e14464f6a54852f7bddff
estebanfern <estebangfernandeza@gmail.com>
2023-01-09 13:30:58 -0300

    Cambio de @RestController a @Controller

--- src/main/java/com/roshka/dtaporteria/contoller/UserController.java
+++ src/main/java/com/roshka/dtaporteria/contoller/UserController.java
@@ -5,10 +5,11 @@ import com.roshka.dtaporteria.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/users")
 public class UserController {
     @Autowired
