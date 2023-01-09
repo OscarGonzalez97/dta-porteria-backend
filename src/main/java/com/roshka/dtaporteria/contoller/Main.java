ae3080430c59e1d21c3cee05067e6bde8289c2b0
Lucas Villagra <lucas-villagra@outlook.com>
2023-01-09 14:50:57 -0300

    Revert "Controller para el dashboard"
    This reverts commit 6e5aadc2487c21f6853352ff2be6ec70293bf6b3.

--- src/main/java/com/roshka/dtaporteria/contoller/Main.java
+++ src/main/java/com/roshka/dtaporteria/contoller/Main.java
@@ -1,16 +1,16 @@
 package com.roshka.dtaporteria.contoller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping
 public class Main {
 
     @GetMapping
     public String helloWorld(){
-        return "dashboard";
+        return "<h1>Hello World!</h1?>";
     }
 
 }
