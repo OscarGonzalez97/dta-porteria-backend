d3b6aed82087166fe7fd101daf98bd4443794221
estebanfern <estebangfernandeza@gmail.com>
2023-01-09 13:43:27 -0300

    @RestController a @Controller en los controladores

--- src/main/java/com/roshka/dtaporteria/contoller/MemberController.java
+++ src/main/java/com/roshka/dtaporteria/contoller/MemberController.java
@@ -5,9 +5,10 @@ import com.roshka.dtaporteria.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/members")
 public class MemberController {
 
