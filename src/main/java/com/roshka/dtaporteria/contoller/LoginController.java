999cc73883038155af61c93d39ec2dd69cc1e7dd
estebanfern <estebangfernandeza@gmail.com>
2023-01-10 15:59:12 -0300

    Controller, template, imgs y scripts del login

---
+++ src/main/java/com/roshka/dtaporteria/contoller/LoginController.java
@@ -0,0 +1,33 @@
+package com.roshka.dtaporteria.contoller;
+
+import com.roshka.dtaporteria.dto.UserDTO;
+import com.roshka.dtaporteria.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public String getLogin(Model model) {
+        model.addAttribute("user", new UserDTO());
+        return "login";
+    }
+    
+    @PostMapping
+    public String postLogin(UserDTO user, Model model) {
+        System.out.println(user);
+        model.addAttribute("user", user);
+        return "dashboard";
+    }
+
+}
