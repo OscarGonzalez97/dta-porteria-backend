9c7b50ba26765177585109488f0049d02085e489
estebanfern <estebangfernandeza@gmail.com>
2023-01-10 15:04:37 -0300

    Limpieza

--- src/main/java/com/roshka/dtaporteria/contoller/Main.java
+++ src/main/java/com/roshka/dtaporteria/contoller/Main.java
@@ -2,8 +2,6 @@ package com.roshka.dtaporteria.contoller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class Main {
