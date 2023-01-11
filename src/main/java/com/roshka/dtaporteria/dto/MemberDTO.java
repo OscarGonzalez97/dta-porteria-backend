dd5c1dc2fde6b1639310126141a889565ee72738
Diego Yegros <diegoyegrosa@gmail.com>
2023-01-11 13:14:26 -0300

    Se modifica texto de Moroso en lista de miembros

--- src/main/java/com/roshka/dtaporteria/dto/MemberDTO.java
+++ src/main/java/com/roshka/dtaporteria/dto/MemberDTO.java
@@ -32,4 +32,5 @@ public class MemberDTO {
             return null;
         }
     }
+
 }
--- src/main/resources/static/js/updateMiembro.js
+++ src/main/resources/static/js/updateMiembro.js
@@ -1,12 +1,10 @@
 const checkbox = document.getElementById("flexCheckDefault")
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-
+var forms = document.querySelectorAll('.needs-validation')
     // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
         .forEach(function (form) {
             form.addEventListener('submit', function (event) {
-                checkbox.value = checkbox.checked
                 if (!form.checkValidity()) {
                     event.preventDefault()
                     event.stopPropagation()
