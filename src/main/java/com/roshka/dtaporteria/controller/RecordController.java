8a241d09c116916274f286f66d4535a74adf96c2
Emanuel-628 <emanuelayalalml@gmail.com>
2023-01-12 15:12:23 -0300

    se agrego la funcion de ver detalle de records

--- src/main/java/com/roshka/dtaporteria/controller/RecordController.java
+++ src/main/java/com/roshka/dtaporteria/controller/RecordController.java
@@ -7,7 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/records")
@@ -21,6 +24,11 @@ public class RecordController {
         return "listRecords";
     }
 
+    @GetMapping("/ver/{id}")
+    public String verDetalles (@PathVariable(value = "id") String id, Model modelo) { //metodo para mostrar los detalles de cada record
+        modelo.addAttribute("records", service.getById(id));
+        return "ver";
+    }
     @GetMapping("/list")
     public ResponseEntity list(){
         return new ResponseEntity(service.list(), HttpStatus.OK);
