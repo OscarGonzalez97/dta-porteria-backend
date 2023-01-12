b7ab66f64e9ec5ee2505f2cea3eb137ecf485f90
Elias Olmedo <elias.olmedor79@gmail.com>
2023-01-12 10:48:13 -0300

    "se agrega bean Frebaseauth"

--- src/main/java/com/roshka/dtaporteria/config/FirebaseInitializer.java
+++ src/main/java/com/roshka/dtaporteria/config/FirebaseInitializer.java
@@ -4,7 +4,10 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -13,9 +16,11 @@ import java.io.IOException;
 
 @Service
 public class FirebaseInitializer {
+
+    @Primary
     @PostConstruct
     private Firestore iniFirestore() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("./firebaseAccountInfo.json");
+        FileInputStream serviceAccount = new FileInputStream("./serviceAccountKey.json");
         FirebaseOptions options = new FirebaseOptions.Builder().
                 setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setDatabaseUrl("https://porteria-dta-test.firebaseio.com/")
@@ -24,7 +29,12 @@ public class FirebaseInitializer {
         return FirestoreClient.getFirestore(firebaseApp);
     }
 
-    public Firestore getFirestore(){
-        return FirestoreClient.getFirestore();
-    }
+    @Bean
+    public Firestore getFirestore() {return FirestoreClient.getFirestore();}
+
+    @Bean
+    public FirebaseAuth getFirebaseAuth(){return FirebaseAuth.getInstance();}
+
+
+
 }
