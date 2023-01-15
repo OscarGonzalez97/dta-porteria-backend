207f7a8e1c0ef5d82a38e580c5eb049b62ca5941
Diego Yegros <diegoyegrosa@gmail.com>
2023-01-15 04:55:31 -0300

    Se cambia @Autowired por Dependency Injection  Constructor-based

--- src/main/java/com/roshka/dtaporteria/controller/MemberController.java
+++ src/main/java/com/roshka/dtaporteria/controller/MemberController.java
@@ -1,18 +1,13 @@
 package com.roshka.dtaporteria.controller;
-
 import com.roshka.dtaporteria.dto.MemberDTO;
 import com.roshka.dtaporteria.reporte.ImportMembersExcel;
 import com.roshka.dtaporteria.service.MemberService;
 import com.roshka.dtaporteria.service.TypeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
@@ -21,12 +16,16 @@ import java.util.Objects;
 @RequestMapping("/members")
 public class MemberController {
 
-    @Autowired
-    private MemberService service;
-    @Autowired
-    private TypeService typeService;
-    @Autowired
-    private ImportMembersExcel importMembersExcel;
+    private final MemberService service;
+    private final TypeService typeService;
+    private final ImportMembersExcel importMembersExcel;
+
+    public MemberController(MemberService service, TypeService typeService, ImportMembersExcel importMembersExcel) {
+        this.service = service;
+        this.typeService = typeService;
+        this.importMembersExcel = importMembersExcel;
+    }
+
     @GetMapping
     public String Miembros(Model model) {
         List<MemberDTO> miembros = service.list();
@@ -34,19 +33,17 @@ public class MemberController {
         model.addAttribute("tipos", typeService.list());
         return "members";
     }
+
     @GetMapping("/add-form")
     public String addForm(Model model){
         model.addAttribute("tipos", typeService.list());
         model.addAttribute("member", new MemberDTO());
         return "newMember";
     }
-    @GetMapping("/{id}")
-    public ResponseEntity getMember(@PathVariable(value = "id") String id){
-        return new ResponseEntity(service.getById(id), HttpStatus.OK);
-    }
+
     @GetMapping("/list")
-    public ResponseEntity listMember(){
-        return new ResponseEntity(service.list(), HttpStatus.OK);
+    public List<MemberDTO> listMember(){
+        return service.list();
     }
 
     @GetMapping("/add")
@@ -58,6 +55,7 @@ public class MemberController {
     public String getMiembroImportarExcel(){
         return "importMiembroExcel";
     }
+
     @PostMapping("/import")
     public String cargarMiembrosExcel(@RequestParam("file") MultipartFile file, RedirectAttributes redirAttr) throws IOException {
         if (file.isEmpty()){
@@ -75,17 +73,13 @@ public class MemberController {
         return "redirect:/members";
     }
 
-    @PostMapping("/add")
-    public ResponseEntity agregarMember(@RequestBody MemberDTO post){
-        return new ResponseEntity(service.add(post), HttpStatus.OK);
-    }
     @PostMapping("/add-form")
     public String agregarFormMember(MemberDTO member) {
         if (!Objects.equals(member.getType(), "Socio"))
         {
             member.setId_member(null);
         }
-        new ResponseEntity(service.add(member), HttpStatus.OK);
+        service.add(member);
         return "redirect:/members";
     }
 
@@ -99,10 +93,7 @@ public class MemberController {
         model.addAttribute("tipos", typeService.list());
         return "updateMember";
     }
-    @PutMapping("/{id}/update")
-    public ResponseEntity editMember(@PathVariable(value = "id") String id, @RequestBody MemberDTO post){
-        return new ResponseEntity(service.edit(id, post), HttpStatus.OK);
-    }
+
     @PostMapping ("/update")
     public String updateMember(MemberDTO member){
         service.update(member);
@@ -111,12 +102,12 @@ public class MemberController {
 
     @PostMapping("/delete/{id}")
     public String deleteMember(@PathVariable(value = "id") String id){
-        new ResponseEntity(service.delete(id), HttpStatus.OK);
+        service.delete(id);
         return "redirect:/members";
     }
     @GetMapping("/delete/{id}")
     public String getDeleteMember(@PathVariable(value = "id") String id){
-        new ResponseEntity(service.delete(id), HttpStatus.OK);
+        service.delete(id);
         return "redirect:/members";
     }
 }
