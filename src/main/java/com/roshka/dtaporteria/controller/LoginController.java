f1a6682faca27abaadad7f9808eeda2a19303d71
Elias Olmedo <elias.olmedor79@gmail.com>
2023-01-16 13:50:17 -0300

    "rutas para templates de olvide mi contrasenha"

--- src/main/java/com/roshka/dtaporteria/controller/LoginController.java
+++ src/main/java/com/roshka/dtaporteria/controller/LoginController.java
@@ -1,22 +1,50 @@
 package com.roshka.dtaporteria.controller;
 
-import com.roshka.dtaporteria.dto.UserDTO;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.roshka.dtaporteria.correo.ServiceMail;
+
 import com.roshka.dtaporteria.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class LoginController {
-    
+
+    @Autowired
+    private FirebaseAuth auth;
     @Autowired
     private UserService service;
+
+    @Autowired
+    private ServiceMail mail;
+
+    @GetMapping("/forgotPass")
+    public String olvideContra() {return "olvideContraseña";}
+
+    @PostMapping("/sendEmail")
+    public String PassResetEmail(@RequestParam("email") String email){
+        String link = null;
+        try {
+            link = auth.generatePasswordResetLink(email);
+        } catch (FirebaseAuthException e) {
+            return "redirect:/forgotPass?err";
+        }
+        Map<String, Object> parametros =new HashMap<>();
+        parametros.put("linkRecuperacion", link);
+
+        mail.sendEmailWithHTML(email, "Cambio de contraseña" , parametros);
+
+        return "redirect:/forgotPass?success";
+    }
 
 
     @GetMapping("/error")
