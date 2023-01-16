136c22a27edec73b91aa28dfc9ce5c20123a182b
Lucas Villagra <lucas-villagra@outlook.com>
2023-01-16 19:17:39 -0300

    creacion de template para olvide contrasena y añadiendo nuevos estilos al Login

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -13,3 +13,24 @@ inputs.forEach(input => {
     input.addEventListener("focus", addcl);
     input.addEventListener("blur", remcl);
 });
+    const esconder = document.querySelector(".olvide-cont");
+const btnolvide = document.querySelector(".forget");
+const primer_form= document.querySelector(".form-primero");
+const btnrecorde = document.querySelector(".recorde");
+const recu= document.querySelector(".recu");
+const msg_recu= document.querySelector(".msg-recu")
+btnolvide.addEventListener("click",()=>{
+    esconder.classList.remove("esconder");
+    esconder.classList.add("mostrar");
+    primer_form.classList.add("esconder");
+})
+btnrecorde.addEventListener("click",()=>{
+    esconder.classList.remove("mostrar");
+    esconder.classList.add("esconder");
+    primer_form.classList.remove("esconder");
+})
+
+recu.addEventListener("click",()=>{
+    btnolvide.classList.add("esconder");
+    msg_recu.style.display=block
+})
