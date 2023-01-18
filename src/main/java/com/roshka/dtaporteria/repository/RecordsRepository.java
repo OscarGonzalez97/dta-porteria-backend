38e282bc198936e2aac0401c142e83abfc40618f
Elias Olmedo <elias.olmedor79@gmail.com>
2023-01-18 14:21:10 -0300

    "repositorio de records"

--- src/main/java/com/roshka/dtaporteria/repository/RecordsRepository.java
+++ src/main/java/com/roshka/dtaporteria/repository/RecordsRepository.java
@@ -2,7 +2,9 @@ package com.roshka.dtaporteria.repository;
 
 import com.roshka.dtaporteria.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecordsRepository extends JpaRepository<Member, String> {
 
 }
