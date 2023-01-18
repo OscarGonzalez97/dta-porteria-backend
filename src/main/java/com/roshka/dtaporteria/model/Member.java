a31c3faa40b7dc2cd4de38d73695ab8a30d1da33
Elias Olmedo <elias.olmedor79@gmail.com>
2023-01-18 14:19:53 -0300

    "Modificacion de primary key "

--- src/main/java/com/roshka/dtaporteria/model/Member.java
+++ src/main/java/com/roshka/dtaporteria/model/Member.java
@@ -1,6 +1,9 @@
 package com.roshka.dtaporteria.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -8,9 +11,9 @@ import java.time.format.DateTimeFormatter;
 
 @Entity
 @Data
-public class Member {
+@NoArgsConstructor
+public class Member{
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String created_by;
     private String ci;
@@ -22,6 +25,18 @@ public class Member {
     private String type;
     private String fecha_vencimiento;
 
+    public Member(String id, String created_by, String ci, String id_member, String is_defaulter, String name, String photo, String surname, String type, String fecha_vencimiento) {
+        this.id = id;
+        this.created_by = created_by;
+        this.ci = ci;
+        this.id_member = id_member;
+        this.is_defaulter = is_defaulter;
+        this.name = name;
+        this.photo = photo;
+        this.surname = surname;
+        this.type = type;
+        this.fecha_vencimiento = fecha_vencimiento;
+    }
     public String getFecha_vencimiento() {
         try {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
